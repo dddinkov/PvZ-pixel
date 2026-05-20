1553ca92836b28f2801d1b38d6fec917965f0a3d
diqn <didkot0334@gmail.com>
2026-05-20 07:14:20 +0300

    Adding a script to cause poison state upon collision and immediately killing the causer afterwards. It's cause damage over time (DOT).

---
+++ Assets/Scripts/PvZ Scripts/PoisonEffect.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisoningSystem : MonoBehaviour
+{
+    [SerializeField]
+    private float dps = 0.5f;
+    [SerializeField]
+    private float duration = 5f;
+    private SpriteRenderer spriteRenderer;
+    private Collider2D trigger;
+
+     void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        trigger = GetComponent<Collider2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Zombie"))
+        {
+            HealthSystem system = other.GetComponent<HealthSystem>();
+            if (system != null)
+            {
+                spriteRenderer.enabled = false;
+                trigger.enabled = false;
+                StartCoroutine(ApplyDamageOverTime(system));
+            }
+        }
+    }
+
+    private IEnumerator ApplyDamageOverTime(HealthSystem system)
+    {
+        ZombieStateHandler stateHandler = system.GetComponent<ZombieStateHandler>();
+        stateHandler?.SetState(ZombieState.Poisoned);
+
+        float time = 0f;
+        while (time < duration)
+        {
+            system.TakeHealth(dps);
+            yield return new WaitForSeconds(1f);
+            time += 1f;
+        }
+
+        stateHandler?.SetState(ZombieState.Normal);
+
+        Destroy(gameObject);
+    }
+}
