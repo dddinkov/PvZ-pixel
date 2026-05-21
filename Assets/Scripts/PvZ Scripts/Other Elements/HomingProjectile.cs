0eda809119429838f36722f306d5041221ab057b
diqn <didkot0334@gmail.com>
2026-05-21 20:10:32 +0300

    Adding HomingProjectile.cs inheriting Projectile. These projectiles follow the nearest enemy to them by rotating towards it.

---
+++ Assets/Scripts/PvZ Scripts/Other Elements/HomingProjectile.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingProjectile : Projectile
+{
+    [SerializeField]
+    private float rotationSpeed = 200f;
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        FindNearestTarget();
+    }
+
+    void FixedUpdate()
+    {
+        if(target == null)
+        {
+            FindNearestTarget();
+        }
+        if(target != null)
+        {
+            Vector2 direction = (Vector2)target.position - (Vector2)transform.position;
+            direction.Normalize();
+
+            float rotateAmount = Vector3.Cross(direction, transform.right).z;
+            rb.angularVelocity = -rotateAmount * rotationSpeed;
+        }
+        rb.velocity = transform.right * speed * Time.deltaTime;
+    }
+
+    void FindNearestTarget()
+    {
+        GameObject[] zombies = GameObject.FindGameObjectsWithTag("Zombie");
+        float closestDistance = Mathf.Infinity;
+        GameObject closestZombie = null;
+
+        foreach (GameObject zombie in zombies)
+        {
+            float distance = Vector2.Distance(transform.position, zombie.transform.position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestZombie = zombie;
+            }
+        }
+
+        if (closestZombie != null)
+        {
+            target = closestZombie.transform;
+        }
+    }
+}
