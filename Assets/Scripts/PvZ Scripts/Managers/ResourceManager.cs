a2e73c453b787fa1bcec1cdb70c775a02c5e3375
diqn <didkot0334@gmail.com>
2026-05-25 06:28:24 +0300

    Adding an universal ResourceManager to handle adding and subtracting from a non-negative resource.

---
+++ Assets/Scripts/PvZ Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System;
+
+public abstract class ResourceManager : MonoBehaviour
+{
+    public event Action<int> OnAmountChanged;
+
+    [SerializeField]
+    protected int startAmount = 0;
+    [SerializeField]
+    protected int maxAmount = 9990;
+
+    public int Amount {get; protected set;}
+
+    protected virtual void Awake()
+    {
+        Amount = startAmount;
+    }
+
+    public virtual void Add(int amount)
+    {
+        Amount = Mathf.Min(Amount + amount, maxAmount);
+
+        OnAmountChanged?.Invoke(Amount);
+    }
+
+    public virtual bool CanSpend(int amount)
+    {
+        return Amount >= amount;
+    }
+
+    public virtual bool TrySpend(int amount)
+    {
+        if(!CanSpend(amount))
+        {
+            return false;
+        }
+
+        Amount -= amount;
+
+        OnAmountChanged?.Invoke(Amount);
+
+        return true;
+    }
+}
